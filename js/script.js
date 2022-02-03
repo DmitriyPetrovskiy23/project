c73c6c82fb106894bc07ec9292f3c0c8a78b50bb
Dmitriy <example@gmail.com>
2022-02-03 14:53:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm('Are you here?');
+
+
+// const answer = prompt('Вам есть 18 лет?','Да');
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?','');
+answers[1] = prompt('Как ваша фамилия?','');
+answers[2] = prompt('Сколько вам лет?','');
+
+document.write(answers);
